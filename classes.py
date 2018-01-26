971a367b8500244393a43bffb613433a56bb9c72
KerneyJ <gamerguy9801@gmail.com>
2018-01-26 08:00:41 -0600

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,51 +1,267 @@
-# version: python3.6
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 20:37:59 2018
 
-from pieces import *
+@author: Jamie
+
+layout
+8
+7
+6
+5
+4
+3
+2
+1
+ a  b  c  d  e  f  g  h
+"""
+
+from ugraphic import *
 
 
+class Piece(object):
 
+    def __init__(self, image= ""):
+        self.image = Image(Point(1,1), image)
+        self.inPlay = True
+
+    def drawSelf(self, win: GraphWin):
+        self.image.draw(win)
+
+    def undrawSelf(self):
+        self.image.undraw()
+
+    def __str__(self):
+        return str(self.image) + " Inplay: " + str(self.inPlay)
+
+
+# Pawns
+
+
+class WhitePawn(Piece):
+
+    def __init__(self):
+        Piece.__init__(self, "Images\WPawn.png")
+
+    def move(self):
+        pass
+
+
+class BlackPawn(Piece):
+
+    def __init__(self):
+        Piece.__init__(self, "Images\BPawn.png")
+
+    def move(self):
+        pass
+
+
+# Rooks
+
+
+class WhiteRook(Piece):
+
+    def __init__(self):
+        Piece.__init__(self, "Images\WRook.png")
+
+    def move(self):
+        pass
+
+
+class BlackRook(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\BRook.png")
+
+    def move(self):
+        pass
+
+# Knight
+
+
+class WhiteKnight(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\WKnight.png")
+
+    def move(self):
+        pass
+
+
+class BlackKnight(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\BKnight.png")
+
+    def move(self):
+        pass
+
+
+# Bishops
+
+
+class WhiteBishop(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\WBishop.png")
+
+    def move(self):
+        pass
+
+
+class BlackBishop(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\BBishop.png")
+
+    def move(self):
+        pass
+# Kings
+
+
+class WhiteKing(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\WKing.png")
+
+    def move(self):
+        pass
+
+
+class BlackKing(Piece):
+    def __init__(self):
+        Piece.__init__(self, "Images\BKing.png")
+
+    def move(self):
+        pass
+
+# Queens
+
+
+class WhiteQueen(Piece):
+
+    def __init__(self):
+        Piece.__init__(self, "Images\WQueen.png")
+
+    def move(self):
+        pass
+
+
+class BlackQueen(Piece):
+
+    def __init__(self):
+        Piece.__init__(self, "Images\BQueen.png")
+
+    def move(self):
+        pass
 
 
 class Player(object):
 
-    def __init__(self, c, b: Board):
-        self.pieces = []
-        self.__givePieces(c)
-        self.board = b
+    def __init__(self, color: str):
+        """Color should be white or black"""
+        # Color stuff
+        self.color = color.lower()
+        if self.color == "white" or self.color == "black":
+            pass
+        else:
+            raise AssertionError("Unexpected value for color", self.color)
 
-    def __givePieces(self, c):
-        if c == 'w':
+        self.pieces = []
+
+    def givePieces(self):
+        if self.color == "white":
             for i in range(8):
                 self.pieces.append(WhitePawn())
 
-            """
             self.pieces.append(WhiteRook())
             self.pieces.append(WhiteRook())
 
             self.pieces.append(WhiteKnight())
             self.pieces.append(WhiteKnight())
 
-            self.pieces.append(WhiteBishop)
-            self.pieces.append(WhiteBishop)
+            self.pieces.append(WhiteBishop())
+            self.pieces.append(WhiteBishop())
 
-            self.pieces.append(WhiteKing)
-            self.pieces.append(WhiteQueen)
-            """
+            self.pieces.append(WhiteKing())
+            self.pieces.append(WhiteQueen())
 
         else:
             for i in range(8):
                 self.pieces.append(BlackPawn())
 
-            self.pieces.append(WhiteRook())
-            self.pieces.append(WhiteRook())
+            self.pieces.append(BlackRook())
+            self.pieces.append(BlackRook())
 
-            self.pieces.append(WhiteKnight())
-            self.pieces.append(WhiteKnight())
+            self.pieces.append(BlackKnight())
+            self.pieces.append(BlackKnight())
 
-            self.pieces.append(WhiteBishop)
-            self.pieces.append(WhiteBishop)
+            self.pieces.append(BlackBishop())
+            self.pieces.append(BlackBishop())
 
-            self.pieces.append(WhiteKing)
-            self.pieces.append(WhiteQueen)
+            self.pieces.append(BlackKing())
+            self.pieces.append(BlackQueen())
 
 
+    def __str__(self):
+        return "Color: " + self.color + " Pieces" + str(self.pieces)
+
+
+class Tile(Rectangle):
+    def __init__(self, p1, p2):
+        Rectangle.__init__(self, p1, p2)
+        self.__pieces = []
+
+    def addPiece(self, p):
+        self.__pieces.append(p)
+
+    def checkCapture(self):
+        if len(self.__pieces) == 2:
+            self.__pieces.pop(0)
+
+    def drawPiece(self, win):
+        dx = self.getCenter().getX() - self.__pieces[0].image.getAnchor().getX()
+        dy = self.getCenter().getY() - self.__pieces[0].image.getAnchor().getY() # Override error for move method
+
+        self.__pieces[0].image.move(dx, dy)
+        self.__pieces[0].image.draw(win)
+
+    def __str__(self):
+        return " Pieces: " + str(self.__pieces)
+
+    def returnPieces(self):
+        return"Pieces: " + str(self.__pieces)
+
+
+class Board(object):
+    def __init__(self, p1: Player, p2: Player):
+        self.player1 = p1
+        self.player2 = p2
+        self.tiles = self.__giveTiles()
+
+    @staticmethod
+    def __giveTiles():
+        tiles = []
+        for i in range(8):
+            temp = []
+            for j in range(8):
+                r = Tile(Point(i * 64, j * 64), Point((i * 64) + 64, (j * 64) + 64))
+                if (i + 1) % 2 == 0 and (j + 1) % 2 == 0:
+                    r.setFill(color_rgb(194, 144, 60))
+                if (j + 1) % 2 != 0 and (i + 1) % 2 != 0:
+                    r.setFill(color_rgb(194, 144, 60))
+
+
+                temp.append(r)
+            tiles.append(temp)
+
+        for i in range(8):
+            tiles[1][i] # Stopping point 7:59 AM 1/26/2018
+        return tiles
+
+    def drawBoard(self, win: GraphWin):
+        for i in self.tiles:
+            for j in i:
+                j.draw(win)
+                j.drawPiece(win)
+
+    def __str__(self):
+        str1 = ""
+        for i in self.tiles:
+            for j in i:
+                str1 += str(j)
+        return "Player1: " + str(self.player1) + "\n" + " Player2: " + str(self.player2) + "\n" + " Tiles: " + str1
--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 from classes import *
 win = GraphWin("Window", 512, 512)
 
-b = Board(1, 1)
-b.drawBoard(win)
-p1 = Player('w', b)
 
-for i in p1.pieces:
-    i.drawSelf(win)
+p1 = Player("white")
+p2 = Player("black")
+b = Board(p1, p2)
+print(b)
+# b.drawBoard(win)
+
+# Demo of adding a piece and drawing it
+# p = WhiteBishop()
+# b.tiles[0][0].addPiece(p)
+# b.tiles[0][0].drawPiece(win)
+
 
 while True:
     k = win.getKey()
