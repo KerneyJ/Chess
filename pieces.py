3c54e125aa5e391cd3238a483c03fd7a274df1e6
KerneyJ <gamerguy9801@gmail.com>
2018-02-08 08:00:46 -0600

    Add files via upload

--- pieces.py
+++ pieces.py
@@ -1,78 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 21 20:37:59 2018
-
-@author: Jamie
-
-layout
-8
-7
-6
-5
-4
-3
-2
-1
- a  b  c  d  e  f  g  h
-"""
-
+from board import *
 from ugraphic import *
-from classes import *
 
-
-class Tile(Rectangle):
-    def __init__(self, x, y):
-        Rectangle.__init__(self, Point(x, y), Point(x + 64, y + 64))
-
-
-class Board(object):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-        # Give tiles to the board
-        self.tiles = self.__giveTiles()
-
-    @staticmethod
-    def __giveTiles():
-        tiles = []
-        for i in range(8):
-            temp = []
-            for j in range(8):
-                r = Rectangle(Point(i * 64, j * 64), Point((i * 64) + 64, (j * 64) + 64))
-                if (i + 1) % 2 == 0 and (j + 1) % 2 == 0:
-                    r.setFill(color_rgb(194, 144, 60))
-                if (j + 1) % 2 != 0 and (i + 1) % 2 != 0:
-                    r.setFill(color_rgb(194, 144, 60))
-
-                temp.append(r)
-            tiles.append(temp)
-
-        return tiles
-
-    def drawBoard(self, win: GraphWin):
-        for i in self.tiles:
-            for j in i:
-                j.draw(win)
-
-    def getWidth(self):
-        return self.width
-
-    def getHeight(self):
-        return self.height
-
-    def setWidth(self, width):
-        self.width = width
-
-    def setHeight(self, height):
-        self.height = height
 
 class Piece(object):
 
-    def __init__(self, point = Tile(1,1), image= ""):
-        self.image = Image(point, image)
+    def __init__(self, image, t):
+        self.image = Image(t.getCenter(), image)
         self.inPlay = True
-        self.position = '1a'
+        self.tile = t
+        self.type = type(self)
+        self.available_tiles = []
 
     def drawSelf(self, win: GraphWin):
         self.image.draw(win)
@@ -80,59 +17,138 @@ class Piece(object):
     def undrawSelf(self):
         self.image.undraw()
 
+    def setTile(self, t):
+        self.tile = t
+        center = self.tile.getCenter()
+        self.image.move(-1 * (self.image.getAnchor().getX() - center.getX()), -1 *(self.image.getAnchor().getY() - center.getY()))
+
+    def __str__(self):
+        return str(self.image) + " Inplay: " + str(self.inPlay) + " Type: " + str(self.type)
+
+
 # Pawns
 
 
 class WhitePawn(Piece):
 
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\WPawn.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\WPawn.png", t)
+        self.amountsMoves = 0
+        print(self.tile.number)
 
-    def move(self):
-        pass
+    def updateAvailableTiles(self, board):
+        # Error every tile is an available tile
+        self.available_tiles = []
+        if self.amountsMoves == 0:
+            for i in board.tiles:
+                for j in i:
+                    if self.tile.number-1 == j.number or self.tile.number-2 == j.number:
+                        self.available_tiles.append(j)
+                        print(j.number, " ", self.tile.number)
+        else:
+            for i in board.tiles:
+                for j in i:
+                    if j.number == self.tile.number - 1:
+                        self.available_tiles.append(j)
+
+        print(self.available_tiles)
+        self.amountsMoves += 1
+
+    def canMove(self, tile):
+        can_move = False
+        for i in self.available_tiles:
+            if i.number == tile.number:
+                can_move = True
+
+        return can_move
 
 
 class BlackPawn(Piece):
 
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\BPawn.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\BPawn.png", t)
+        self.amountMoves = 0
 
-    def move(self):
-        pass
+    def updateAvailableTiles(self, board):
+        self.available_tiles = []
+        if self.amountMoves == 0:
+            for i in board.tiles:
+                for j in i:
+                    if self.tile.number + 1 == j.number or self.tile.number + 2 == j.number:
+                        self.available_tiles.append(j)
+                        print(j.number, " ", self.tile.number)
+        else:
+            for i in board.tiles:
+                for j in i:
+                    if j.number == self.tile.number + 1:
+                        self.available_tiles.append(j)
+
+        print(self.available_tiles)
+        self.amountMoves += 1
+
+    def canMove(self, tile):
+        can_move = False
+        for i in self.available_tiles:
+            if i.number == tile.number:
+                can_move = True
+
+        return can_move
+
 
 # Rooks
 
 
 class WhiteRook(Piece):
 
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\WRook.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\WRook.png", t)
 
-    def move(self):
-        pass
+    def updateAvailableTiles(self, board):
+        self.available_tiles = []
+        for i in board.tiles:
+            for j in i:
+                pass
+
+
+    def canMove(self, tile):
+        can_move = False
+        for i in self.available_tiles:
+            if i.number == tile.number:
+                can_move = True
+
+        return can_move
 
 
 class BlackRook(Piece):
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\BRook.png")
 
-    def move(self):
-        pass
+    def __init__(self, t):
+        Piece.__init__(self, "Images\BRook.png", t)
+
+    def canMove(self, tile):
+        can_move = False
+        for i in self.available_tiles:
+            if i.number == tile.number:
+                can_move = True
+
+        return can_move
+
 
 # Knight
 
 
 class WhiteKnight(Piece):
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\WKnight.png")
+
+    def __init__(self, t):
+        Piece.__init__(self, "Images\WKnight.png", t)
 
     def move(self):
         pass
 
 
 class BlackKnight(Piece):
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\BKnight.png")
+
+    def __init__(self, t):
+        Piece.__init__(self, "Images\BKnight.png", t)
 
     def move(self):
         pass
@@ -142,44 +158,48 @@ class BlackKnight(Piece):
 
 
 class WhiteBishop(Piece):
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\WBishop.png")
+
+    def __init__(self, t):
+        Piece.__init__(self, "Images\WBishop.png", t)
 
     def move(self):
         pass
 
 
 class BlackBishop(Piece):
-    def __init__(self, point=Tile(1, 1)):
-        Piece.__init__(self, point, "Images\BBishop.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\BBishop.png", t)
 
     def move(self):
         pass
+
+
 # Kings
 
 
 class WhiteKing(Piece):
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\WKing.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\WKing.png", t)
 
     def move(self):
         pass
 
 
 class BlackKing(Piece):
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\BKing.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\BKing.png", t)
 
     def move(self):
         pass
+
 
 # Queens
 
 
 class WhiteQueen(Piece):
 
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\WQueen.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\WQueen.png", t)
 
     def move(self):
         pass
@@ -187,8 +207,8 @@ class WhiteQueen(Piece):
 
 class BlackQueen(Piece):
 
-    def __init__(self, point = Tile(1,1)):
-        Piece.__init__(self, point, "Images\BQueen.png")
+    def __init__(self, t):
+        Piece.__init__(self, "Images\BQueen.png", t)
 
     def move(self):
         pass
